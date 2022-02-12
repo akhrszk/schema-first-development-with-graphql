d47975ac7308a8f760bcc795d0e5ebf5a2fde4ab
Akihiro Suzuki <28677705+akhrszk@users.noreply.github.com>
2022-02-13 04:12:00 +0900

    :wrench: change graphql document

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals'
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 })
 
 ReactDOM.render(
